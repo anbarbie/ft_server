fdda7958e8f1c345687bb394a02e84606d6f49ee
anbarbie <siliose@hotmail.fr>
2021-08-03 21:09:01 +0200

    typo

--- srcs/autoindex.sh
+++ srcs/autoindex.sh
@@ -1,2 +1,2 @@
-sed -i "s/autindex on;/autoindex off;/g" /etc/nginx/sites-available/ngninx.config
+sed -i "s/autindex on;/autoindex off;/g" /etc/nginx/sites-available/nginx.config
 service nginx restart
